dfed8b8580f874ec40a6204da36ad8126fa2bd36
Kumar Shashank <oo.ks.pp@gmail.com>
2026-07-02 12:36:49 +0530

    fix: gcs lifecycle_rule action multi-line syntax

--- terraform/modules/gcs/main.tf
+++ terraform/modules/gcs/main.tf
@@ -4,16 +4,23 @@ resource "google_storage_bucket" "data_lake" {
   location      = var.location
   force_destroy = true
 
-  # Lifecycle: move objects older than 30 days to cheaper storage
   lifecycle_rule {
-    condition { age = 30 }
-    action    { type = "SetStorageClass", storage_class = "NEARLINE" }
+    condition {
+      age = 30
+    }
+    action {
+      type          = "SetStorageClass"
+      storage_class = "NEARLINE"
+    }
   }
 
-  # Lifecycle: delete objects older than 90 days
   lifecycle_rule {
-    condition { age = 90 }
-    action    { type = "Delete" }
+    condition {
+      age = 90
+    }
+    action {
+      type = "Delete"
+    }
   }
 
   uniform_bucket_level_access = true
